ec2aca600c62787c16b2f659153152e4b27b10c9
Govind.S.B <b.s.dnivog@gmail.com>
2020-12-01 09:23:27 +0530

    Moved codebase from sql to sqlite . And added a mock Profile chooser
    we moved to Sqllite since its built in to python , and does not require the client to have any additional requirements

--- Main.py
+++ Main.py
@@ -1,40 +1,36 @@
-import mysql.connector
+import sqlite3 # Instead of sql , we usesqlite its more or less the same as sql
 
-try:  #checks if password and username have been given before
-    with open('password.txt','r') as file:
-        info = dict(user=file.readline(), passwd=file.readline())
+#-----------------------------------------
+# To obtain list of .db files
 
+import glob
+import os
 
-except FileNotFoundError:  #if the username and password has not been given yet
-    with open('password.txt', 'w') as file:
+os.chdir(r'.\Databases') #this changes working dir to database folder 
+DatabaseList = glob.glob('*.db')
+#-----------------------------------------
+# Profile Choosing Or Creatiion [I should make a dlete function but thats for later]
 
-        username = input('enter SQL username: ')
-        password = input('enter SQL password: ')
+if len(DatabaseList)== 0 :
+    print("No Profiles Created")
+    Profile = input("Enter new profile name to continue\n")+ ".db" 
+else :
+    print("Currently Active Profiles\n",DatabaseList)
+    
+    Tempvar1 = input("Enter Profile Index ,to make new profile type +\n") # just a temp variable that ill del later
+    if Tempvar1 == "+":
+        Profile = input("Enter new profile name to continue\n")+ ".db" 
+    else:
+        Profile = DatabaseList[int(Tempvar1)]
+        
+    del Tempvar1 # Deleting Temp Variable 
 
-        file.write(username + '\n')
+db = sqlite3.connect(Profile)
 
-        file.write(password)
-        info = {
-            'user': username,
-            'passwd': password
-        }
+#-----------------------------------------
 
-db = mysql.connector.connect(
-    host="localhost",
-    user=info['user'],
-    passwd=info['passwd'],
-)
+os.chdir(r'..\\') # Setting Back working dir to project folder
 
 mycursor = db.cursor()
 
-mycursor.execute('show databases')
 
-
-for database in mycursor:
-    if database[0] == 'budget_planner':
-        mycursor.fetchall()  #to clear any extra stuff in the cursor, otherwise we cant execute anything else.
-        break
-else:
-    mycursor.execute('CREATE DATABASE Budget_Planner')
-
-mycursor.execute('USE Budget_Planner')
