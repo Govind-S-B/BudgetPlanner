f0c81cf12835490bc38d43e40f85f871c2e21811
officiallyaninja <officiallyaninja@gmail.com>
2020-12-01 12:04:59 +0530

    Cleaned up code.
    made it a bit neater and added a few comments. also added a text file so \databases will now be on Git

--- Main.py
+++ Main.py
@@ -1,36 +1,36 @@
 import sqlite3 # Instead of sql , we usesqlite its more or less the same as sql
-
-#-----------------------------------------
-# To obtain list of .db files
-
-import glob
+import glob # To obtain list of .db files
 import os
 
+
 os.chdir(r'.\Databases') #this changes working dir to database folder 
-DatabaseList = glob.glob('*.db')
-#-----------------------------------------
-# Profile Choosing Or Creatiion [I should make a dlete function but thats for later]
+database_list = glob.glob('*.db')  #gets a list of all files with a .db extension in \Databases
 
-if len(DatabaseList)== 0 :
-    print("No Profiles Created")
-    Profile = input("Enter new profile name to continue\n")+ ".db" 
-else :
-    print("Currently Active Profiles\n",DatabaseList)
+# profile Choosing Or Creation
+# TODO: add a way to delete profiles
+
+if len(database_list)== 0:  # checks if a database already exists
+    print("No profiles Created")
+    profile = input("Enter new profile name to continue\n")+ ".db"
+else:
+    print("Currently Active profiles:")
+    for i in range(len(database_list)):
+        database = database_list[i]
+        print(f'[{i}]'+database.replace('.db',''))
     
-    Tempvar1 = input("Enter Profile Index ,to make new profile type +\n") # just a temp variable that ill del later
-    if Tempvar1 == "+":
-        Profile = input("Enter new profile name to continue\n")+ ".db" 
+    profile_choice = input("Enter profile Index ,to make new profile type +\n") # just a temp variable that ill del later
+    if profile_choice == "+":
+        profile = input("Enter new profile name to continue\n")+ ".db"
     else:
-        Profile = DatabaseList[int(Tempvar1)]
+        profile = database_list[int(profile_choice)]
         
-    del Tempvar1 # Deleting Temp Variable 
+    del profile_choice # Deleting Temp Variable
 
-db = sqlite3.connect(Profile)
-
-#-----------------------------------------
+db = sqlite3.connect(profile)  # creates the .db file
 
 os.chdir(r'..\\') # Setting Back working dir to project folder
-
+#-----------------------------------------
+# from here is normal SQL
 mycursor = db.cursor()
 
 
