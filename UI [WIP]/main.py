8e2a24152b074eeae7c41fb569ec2d146ce681fe
Govind.S.B <b.s.dnivog@gmail.com>
2020-12-08 21:14:11 +0530

    Made a base ui
    made a base ui (more like immitating his tutorial but ye , now the outer ui is kinda done . test it out)
    btw pip install pyqt5 to test shiz
    and if u want to use the desiger pip install pyqt5-tools

---
+++ UI [WIP]/main.py
@@ -0,0 +1,122 @@
+import sys
+import platform
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+
+from ui_main import Ui_MainWindow # IMPORT GUI FILE
+
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.ui = Ui_MainWindow() # so here i think the gui is inside this main window
+        self.ui.setupUi(self)
+
+        # MOVE WINDOW
+        def moveWindow(event):
+            # RESTORE BEFORE MOVE
+            if UIFunctions.returnStatus() == 1:
+                UIFunctions.maximize_restore(self)
+
+            # IF LEFT CLICK MOVE WINDOW
+            if event.buttons() == Qt.LeftButton:
+                self.move(self.pos() + event.globalPos() - self.dragPos)
+                self.dragPos = event.globalPos()
+                event.accept()
+
+        # SET TITLE BAR
+        self.ui.title_bar.mouseMoveEvent = moveWindow
+
+        ## ==> SET UI DEFINITIONS
+        UIFunctions.uiDefinitions(self)
+
+
+        ## SHOW ==> MAIN WINDOW
+        ########################################################################
+        self.show()
+
+    ## APP EVENTS
+    ########################################################################
+    def mousePressEvent(self, event):
+        self.dragPos = event.globalPos()
+
+
+
+#_________________________________________________________________________________
+GLOBAL_STATE = 0
+
+class UIFunctions(MainWindow):
+
+    ## ==> MAXIMIZE RESTORE FUNCTION
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        status = GLOBAL_STATE
+
+        # IF NOT MAXIMIZED
+        if status == 0:
+            self.showMaximized()
+
+            # SET GLOBAL TO 1
+            GLOBAL_STATE = 1
+
+            # IF MAXIMIZED REMOVE MARGINS AND BORDER RADIUS
+            self.ui.drop_shadow_layout.setContentsMargins(0, 0, 0, 0)
+            self.ui.drop_shadow_frame.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0.0284091, y1:0.045, x2:0.960227, y2:0.972, stop:0 rgba(75, 108, 183, 255), stop:0.806818 rgba(24, 40, 72, 255)); border-radius: 0px;")
+            self.ui.bt_maximise.setToolTip("Restore")
+        else:
+            GLOBAL_STATE = 0
+            self.showNormal()
+            self.resize(self.width()+1, self.height()+1)
+            self.ui.drop_shadow_layout.setContentsMargins(10, 10, 10, 10)
+            self.ui.drop_shadow_frame.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0.0284091, y1:0.045, x2:0.960227, y2:0.972, stop:0 rgba(75, 108, 183, 255), stop:0.806818 rgba(24, 40, 72, 255)); border-radius: 10px;")
+            self.ui.bt_maximise.setToolTip("Maximize")
+
+    ## ==> UI DEFINITIONS
+    def uiDefinitions(self):
+
+        # REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        # SET DROPSHADOW WINDOW
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 100))
+
+        # APPLY DROPSHADOW TO FRAME
+        self.ui.drop_shadow_frame.setGraphicsEffect(self.shadow)
+
+        # MAXIMIZE / RESTORE
+        self.ui.bt_maximise.clicked.connect(lambda: UIFunctions.maximize_restore(self))
+
+        # MINIMIZE
+        self.ui.bt_minimise.clicked.connect(lambda: self.showMinimized())
+
+        # CLOSE
+        self.ui.bt_close.clicked.connect(lambda: self.close())
+
+        ## ==> CREATE SIZE GRIP TO RESIZE WINDOW
+        self.sizegrip = QSizeGrip(self.ui.frame_grip)
+        self.sizegrip.setStyleSheet("QSizeGrip { width: 10px; height: 10px; margin: 5px } QSizeGrip:hover { background-color: rgb(50, 42, 94) }")
+        self.sizegrip.setToolTip("Resize Window")
+
+
+
+    ## RETURN STATUS IF WINDOWS IS MAXIMIZE OR RESTAURED
+    def returnStatus():
+        return GLOBAL_STATE
+#_________________________________________________________________________________
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())
